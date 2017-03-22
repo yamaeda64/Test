094420849c632220c292906c13551a5fe0dd5cb3
yamaeda64 <joakim_bergqvist@hotmail.com>
2017-03-22 22:30:45 +0100

    new SmallClass

---
+++ module/src/SmallClass.java
@@ -0,0 +1,44 @@
+
+public class SmallClass
+{
+    private String title;
+    private String description;
+    private int counter;
+    
+    SmallClass()
+    {
+        title = "";
+        description = "";
+        counter = 0;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+    
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    
+    public int getCounter()
+    {
+        return counter;
+    }
+    
+    public void setCounter(int counter)
+    {
+        this.counter = counter;
+    }
+}
