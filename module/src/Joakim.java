a18dd36070c6961d82a9dbbc68691383688b4752
yamaeda64 <joakim_bergqvist@hotmail.com>
2017-03-24 12:28:24 +0100

    change to Class Joakim

--- module/src/Joakim.java
+++ module/src/Joakim.java
@@ -2,9 +2,10 @@
 public class Joakim
 {
     private String name;
-    
+    private String title;
     public Joakim()
     {
         name = "Joakim";
+        title = "Project Manager";
     }
 }
